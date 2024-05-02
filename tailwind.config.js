78201b9b4ce54188105277efe1a8cc54f7b6ef76
Valdeir Rodrigues <vardii.rodrigues@gmail.com>
2024-05-02 10:25:22 -0400

    add colors and typography and apply to the root base

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily:{
+      jakarta: ["Plus Jakarta Sans", "sans-serif"]
+    },
+    extend: {
+      colors: {
+        lightBlue: '#666CA3',
+        darkBlue: '#13183f',
+        MyGray: '#83869a',
+        pinkChiclete: '#f74780',
+        pinkRose: '#ffa7c3',
+        btnPink: '#f02aa6',
+        btnOrange2: '#ff6f48',
+        btnBlue: '#4851ff',
+      }
+    },
   },
   plugins: [],
 }
