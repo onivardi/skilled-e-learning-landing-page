8a56c186ed0a4bc6650a7bdb15369d9f1cd8dc23
Valdeir Rodrigues <vardii.rodrigues@gmail.com>
2024-05-03 09:45:12 -0400

    fix bg hero image layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,31 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    fontFamily:{
-      jakarta: ["Plus Jakarta Sans", "sans-serif"]
+    fontFamily: {
+      jakarta: ["Plus Jakarta Sans", "sans-serif"],
     },
-    
+    screens: {
+      tablet: "640px",
+      laptop: "1024px",
+      desktop: "1280px",
+    },
     extend: {
       colors: {
-        lightBlue: '#666CA3',
-        darkBlue: '#13183f',
-        MyGray: '#83869a',
-        pinkChiclete: '#f74780',
-        pinkRose: '#ffa7c3',
-        btnPink: '#f02aa6',
-        btnOrange2: '#ff6f48',
-        btnBlue: '#4851ff',
+        lightBlue: "#666CA3",
+        darkBlue: "#13183f",
+        MyGray: "#83869a",
+        pinkChiclete: "#f74780",
+        pinkRose: "#ffa7c3",
+        btnPink: "#f02aa6",
+        btnOrange2: "#ff6f48",
+        btnBlue: "#4851ff",
       },
       backgroundImage: {
-      'hero-table': "url('../assets/image-hero-tablet.png')"
-    },
+        "hero-mobile": "url('../assets/image-hero-mobile.png')",
+        "hero-table": "url('../assets/image-hero-tablet.png')",
+        "hero-desktop": "url('../assets/image-hero-desktop.png')",
+      },
     },
   },
   plugins: [],
-}
+};
